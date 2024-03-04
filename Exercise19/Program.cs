dd47ff15682ceafc30d483a833eb8f578c7487d1
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:53:00 +0330

    Add project files.

---
+++ Exercise19/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise19
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string a = Console.ReadLine();
+            var result1 = Convert.ToInt32(a);
+            var result2 = Negative(result1);
+            Console.WriteLine("Negative of your number is:" + result2);
+        }
+
+        public static int Negative(int a)
+        {
+            return a *= -1;
+        }
+    }
+
+}
